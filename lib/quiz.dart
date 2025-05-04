90983d47bb39d04c5d6aecd06417dd759ef089f4
tunmdev <tunmdevv@gmail.com>
2025-05-04 16:05:52 +0700

    Initial project

---
+++ lib/quiz.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/questions_screen.dart';
+import 'package:flutter_application_1/screen/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 51, 14, 116),
+              Color.fromARGB(255, 111, 41, 231)
+            ], begin: Alignment.centerLeft),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
